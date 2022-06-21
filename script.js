508224b9ea1bc4dc04aaab581cc6554d5e889d18
Endy3032 <>
2022-06-21 13:10:45 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,2 @@
+// const { data } = await axios.get(encodeURI("https://api.weatherapi.com/v1/forecast.json"), { params: { key: "3d35407e30c24f8eb3d102228212307", q: "Ho Chi Minh City,Vietnam", days: 1, aqi: "yes" } })
+// console.log(data)
