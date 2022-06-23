9a75910c8e68e4ccdc938449186c3265b5004210
Endy3032 <61771609+Endy3032@users.noreply.github.com>
2022-06-23 19:47:30 +0700

    Camera UI & YouTube

--- script.js
+++ script.js
@@ -1,18 +1,31 @@
 // const { data } = await axios.get(encodeURI("https://api.weatherapi.com/v1/forecast.json"), { params: { key: "3d35407e30c24f8eb3d102228212307", q: "Ho Chi Minh City,Vietnam", days: 1, aqi: "yes" } })
 // console.log(data)
 var countdown
-time = () => {
-  const date = new Date();
-  const timeMsg = `Bây giờ là ${date.toLocaleString("vi", { hour: "numeric", minute: "numeric", second: "numeric" })}`;
+
+showOverlay = (bgImage = null) => {
   const overlay = document.querySelector(".overlay")
   overlay.style.opacity = 1
   overlay.style.zIndex = 99
+  if (bgImage) overlay.style.backgroundImage = bgImage
+  return overlay
+}
+
+time = () => {
+  const date = new Date();
+  const timeMsg = `Bây giờ là ${date.toLocaleString("vi", { hour: "numeric", minute: "numeric", second: "numeric" })}`;
+  const overlay = showOverlay()
   overlay.innerText = timeMsg
   countdown = setTimeout(time, 1000 - date.getMilliseconds());
 }
 
+cam = () => {
+  document.querySelector(".camera").style.opacity = 1
+  showOverlay("url(Resources/camera.png)")
+}
+
 youtube = () => {
-  document.getElementById("screen").innerHTML = `<iframe src="https://www.youtube-nocookie.com/embed/HSsqzzuGTPo?controls=0&amp;start=5&autoplay=1" allow="autoplay; encrypted-media; picture-in-picture" allowfullscreen></iframe>`
+  const overlay = showOverlay()
+  overlay.innerHTML = `<iframe src="https://www.youtube-nocookie.com/embed/HSsqzzuGTPo?controls=0&amp;start=5&autoplay=1" allow="autoplay; encrypted-media; picture-in-picture" allowfullscreen></iframe>`
 }
 
 updateStatusbarTime = () => {
@@ -24,9 +37,11 @@ updateStatusbarTime = () => {
 updateStatusbarTime()
 
 home = () => {
+  document.querySelector(".camera").style.opacity = 0
   const overlay = document.querySelector(".overlay")
   overlay.style.opacity = 0
   overlay.style.zIndex = 0
   overlay.innerHTML = ""
+  overlay.style.backgroundImage = "none"
   clearTimeout(countdown)
 }
