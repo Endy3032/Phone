ba0c5489660749f6c46cb00e8fbfee84ef1deb37
Endy <61771609+Endy3032@users.noreply.github.com>
2022-06-23 09:31:12 +0000

    Updates

--- script.js
+++ script.js
@@ -1,2 +1,11 @@
 // const { data } = await axios.get(encodeURI("https://api.weatherapi.com/v1/forecast.json"), { params: { key: "3d35407e30c24f8eb3d102228212307", q: "Ho Chi Minh City,Vietnam", days: 1, aqi: "yes" } })
 // console.log(data)
+function time() {
+  const date = new Date();
+  var timeMsg = `Bây giờ là ${date.getHours()} , ${date.getMinutes()} , ${date.getSeconds()}`;
+  document.getElementById("screen").innerText = timeMsg;
+  var countdown = setInterval(time, 1000);
+}
+function youtube(){
+    document.getElementById("screen").innerHTML = `<iframe src="https://youtube.com/embed/oxbqvhiJktc&ab"></iframe>`
+}
